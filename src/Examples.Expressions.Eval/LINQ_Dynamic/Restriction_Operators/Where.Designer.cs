08c76a3e81527fa4f5f172dd51a264bf413a2a1d
zzzprojects <sales@zzzprojcts.com>
2017-04-08 09:17:29 -0400

    Add a second example
    Add a second example

--- src/Examples.Expressions.Eval/LINQ_Dynamic/Restriction_Operators/Where.Designer.cs
+++ src/Examples.Expressions.Eval/LINQ_Dynamic/Restriction_Operators/Where.Designer.cs
@@ -34,8 +34,13 @@
             this.uiWhere_Simple_1_Execute = new System.Windows.Forms.Button();
             this.uiWhere_Simple_1_LINQ_Dynamic = new System.Windows.Forms.Button();
             this.uiWhere_Simple_1_LINQ = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.uiWhere_Simple_2_Execute = new System.Windows.Forms.Button();
+            this.uiWhere_Simple_2_LINQ_Dynamic = new System.Windows.Forms.Button();
+            this.uiWhere_Simple_2_LINQ = new System.Windows.Forms.Button();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox2
@@ -98,17 +103,61 @@
             this.uiWhere_Simple_1_LINQ.UseVisualStyleBackColor = true;
             this.uiWhere_Simple_1_LINQ.Click += new System.EventHandler(this.uiWhere_Simple_1_LINQ_Click);
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.uiWhere_Simple_2_Execute);
+            this.groupBox3.Controls.Add(this.uiWhere_Simple_2_LINQ_Dynamic);
+            this.groupBox3.Controls.Add(this.uiWhere_Simple_2_LINQ);
+            this.groupBox3.Location = new System.Drawing.Point(140, 10);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(120, 135);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Where - Simple 2";
+            // 
+            // uiWhere_Simple_2_Execute
+            // 
+            this.uiWhere_Simple_2_Execute.Location = new System.Drawing.Point(10, 100);
+            this.uiWhere_Simple_2_Execute.Name = "uiWhere_Simple_2_Execute";
+            this.uiWhere_Simple_2_Execute.Size = new System.Drawing.Size(100, 23);
+            this.uiWhere_Simple_2_Execute.TabIndex = 5;
+            this.uiWhere_Simple_2_Execute.Text = "Execute";
+            this.uiWhere_Simple_2_Execute.UseVisualStyleBackColor = true;
+            this.uiWhere_Simple_2_Execute.Click += new System.EventHandler(this.uiWhere_Simple_2_Execute_Click);
+            // 
+            // uiWhere_Simple_2_LINQ_Dynamic
+            // 
+            this.uiWhere_Simple_2_LINQ_Dynamic.Location = new System.Drawing.Point(10, 60);
+            this.uiWhere_Simple_2_LINQ_Dynamic.Name = "uiWhere_Simple_2_LINQ_Dynamic";
+            this.uiWhere_Simple_2_LINQ_Dynamic.Size = new System.Drawing.Size(100, 23);
+            this.uiWhere_Simple_2_LINQ_Dynamic.TabIndex = 4;
+            this.uiWhere_Simple_2_LINQ_Dynamic.Text = "LINQ Dynamic";
+            this.uiWhere_Simple_2_LINQ_Dynamic.UseVisualStyleBackColor = true;
+            this.uiWhere_Simple_2_LINQ_Dynamic.Click += new System.EventHandler(this.uiWhere_Simple_2_LINQ_Dynamic_Click);
+            // 
+            // uiWhere_Simple_2_LINQ
+            // 
+            this.uiWhere_Simple_2_LINQ.Location = new System.Drawing.Point(10, 20);
+            this.uiWhere_Simple_2_LINQ.Name = "uiWhere_Simple_2_LINQ";
+            this.uiWhere_Simple_2_LINQ.Size = new System.Drawing.Size(100, 23);
+            this.uiWhere_Simple_2_LINQ.TabIndex = 3;
+            this.uiWhere_Simple_2_LINQ.Text = "LINQ";
+            this.uiWhere_Simple_2_LINQ.UseVisualStyleBackColor = true;
+            this.uiWhere_Simple_2_LINQ.Click += new System.EventHandler(this.uiWhere_Simple_2_LINQ_Click);
+            // 
             // Where
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(771, 405);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Where";
             this.Text = "Where";
             this.groupBox2.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -121,5 +170,9 @@
         private System.Windows.Forms.Button uiWhere_Simple_1_Execute;
         private System.Windows.Forms.Button uiWhere_Simple_1_LINQ_Dynamic;
         private System.Windows.Forms.Button uiWhere_Simple_1_LINQ;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button uiWhere_Simple_2_Execute;
+        private System.Windows.Forms.Button uiWhere_Simple_2_LINQ_Dynamic;
+        private System.Windows.Forms.Button uiWhere_Simple_2_LINQ;
     }
 }
--- src/Examples.Expressions.Eval/LINQ_Dynamic/Restriction_Operators/Where.cs
+++ src/Examples.Expressions.Eval/LINQ_Dynamic/Restriction_Operators/Where.cs
@@ -35,7 +35,7 @@ namespace Examples.Expressions.Eval.LINQ_Dynamic.Restriction_Operators
 
         private void uiWhere_Simple_1_LINQ_Dynamic_Click(object sender, EventArgs e)
         {
-            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            int[] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2, 0};
 
             var lowNums = numbers.Where(n => "n < 5");
 
@@ -52,7 +52,7 @@ namespace Examples.Expressions.Eval.LINQ_Dynamic.Restriction_Operators
 
         private void uiWhere_Simple_1_Execute_Click(object sender, EventArgs e)
         {
-            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            int[] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2, 0};
 
             var lowNums = numbers.Execute<IEnumerable<int>>("Where(n => n < 5)");
 
@@ -62,6 +62,61 @@ namespace Examples.Expressions.Eval.LINQ_Dynamic.Restriction_Operators
             foreach (var x in lowNums)
             {
                 sb.AppendLine(x.ToString());
+            }
+
+            My.ShowResult(My.LinqResultType.Execute, uiResult, sb);
+        }
+
+        #endregion
+
+        #region Where_Simple_2
+
+        private void uiWhere_Simple_2_LINQ_Click(object sender, EventArgs e)
+        {
+            var products = My.GetProductList();
+
+            var soldOutProducts = products.Where(prod => prod.UnitsInStock == 0);
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Sold out products:");
+            foreach (var product in soldOutProducts)
+            {
+                sb.AppendLine("{0} is sold out!", product.ProductName);
+            }
+
+            My.ShowResult(My.LinqResultType.Linq, uiResult, sb);
+        }
+
+        private void uiWhere_Simple_2_LINQ_Dynamic_Click(object sender, EventArgs e)
+        {
+            var products = My.GetProductList();
+
+            var soldOutProducts = products.Where(prod => "prod.UnitsInStock == 0");
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Sold out products:");
+            foreach (var product in soldOutProducts)
+            {
+                sb.AppendLine("{0} is sold out!", product.ProductName);
+            }
+
+            My.ShowResult(My.LinqResultType.LinqDynamic, uiResult, sb);
+        }
+
+        private void uiWhere_Simple_2_Execute_Click(object sender, EventArgs e)
+        {
+            var products = My.GetProductList();
+
+            var soldOutProducts = products.Execute<IEnumerable<My.Product>>("Where(prod => prod.UnitsInStock == 0)");
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Sold out products:");
+            foreach (var product in soldOutProducts)
+            {
+                sb.AppendLine("{0} is sold out!", product.ProductName);
             }
 
             My.ShowResult(My.LinqResultType.Execute, uiResult, sb);
